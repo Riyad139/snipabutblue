94d5cf83227e717ab39a89d79d5977ce83c5e014
Riyad139 <riyad.ahmed1886@gmail.com>
2024-04-02 03:50:25 +0600

    ade

--- components/AboutUs/AboutUs.tsx
+++ components/AboutUs/AboutUs.tsx
@@ -4,8 +4,8 @@ export function AboutUs() {
   return (
     <div className="flex md:flex-row flex-col px-5 border-b-4 border-white pt-10 pb-20">
       <div className="flex md:mt-0 mt-20  text-darkBlue flex-col justify-center">
-        <h2 className="text-7xl font-semibold">About</h2>
-        <h2 className="text-7xl font-semibold">sinpabutblue</h2>
+        <h2 className=" text-5xl sm:text-7xl font-semibold">About</h2>
+        <h2 className=" text-4xl sm:text-7xl font-semibold">sinpabutblue</h2>
         <p className="text-2xl mt-7">
           The iconic green frog that took the crypto world by storm.Well,{" "}
           {"that's"}
