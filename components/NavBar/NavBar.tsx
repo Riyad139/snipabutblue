d0beb425daf9e413c2ea9d4b1f2842fa072a090f
Riyad139 <riyad.ahmed1886@gmail.com>
2024-04-02 03:41:32 +0600

    added

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -4,13 +4,12 @@ export default function NavBar() {
   const linkItems = [
     { name: "About", id: "about" },
     { name: "How To Buy", id: "buy" },
-    { name: "Tokenomics", id: "Tokenomics" },
   ];
 
   return (
     <div className="py-3 px-5 border-b border-white bg-[#333A73] w-full">
       <div className="max-w-[90rem] flex items-center justify-between mx-auto">
-        <div className="w-28">
+        <div className=" w-20 sm:w-28">
           <Image
             src={"/apeblue.png"}
             width={1920}
@@ -19,7 +18,7 @@ export default function NavBar() {
             alt="pepe icon"
           />
         </div>
-        <ul className="flex space-x-5 text-white text-2xl font-semibold">
+        <ul className="flex space-x-5 text-white text-xl sm:text-2xl font-semibold">
           {linkItems.map((item) => (
             <li className="flex" key={item.id}>
               {item.name}
