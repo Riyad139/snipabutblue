9f8521a79e50d1686760ba6bbd22514acddd1dd9
Riyad139 <riyad.ahmed1886@gmail.com>
2024-04-02 05:00:34 +0600

    added

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { FaTelegram } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="py-20 text-darkBlue  flex flex-col items-center">
-      <h3 className="text-5xl font-semibold">snipabutblue</h3>
+      <h3 className="text-5xl font-semibold">$Snipa</h3>
       <div className="flex gap-4 mt-16">
         <Link
           className="text-3xl p-3 bg-black text-white rounded-lg"
